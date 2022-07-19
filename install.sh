9962cdbee7098b1d636075604559826c78c7a3eb
Baba01hacker69 <109589077+Baba01hacker69@users.noreply.github.com>
2022-07-19 18:19:37 +0545

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+pkg install x11-repo
+pkg install root-repo
+apt update && apt upgrade
+pkg install wget git nmap python python2 
+pkg install zenmap
+pkg install tsu
+pkg install ruby
+pip install lolcat
+pip install Requests
+pip install pygame
+apt install aircrack-ng
+
